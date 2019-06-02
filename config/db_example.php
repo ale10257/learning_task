f575e17705073ae748cef6f4c64207ea6f3cdb64
Alexey Kulagin <proctoleha@gmail.com>
2019-06-02 07:47:47 +0300

    fix

--- config/db_example.php
+++ config/db_example.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.01:3306;dbname=testDB',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=testDB',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
