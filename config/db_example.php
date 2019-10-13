036ddd6fba723e6e3666bb14365d241a671b248b
ale10257 <proctoleha@gmail.com>
2019-10-13 10:26:03 +0300

    fix

--- config/db_example.php
+++ config/db_example.php
@@ -6,9 +6,4 @@ return [
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
