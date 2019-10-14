02ab9548438593d5926a4c037dbbd2902e44b51b
ale10257 <proctoleha@gmail.com>
2019-10-14 12:52:18 +0300

    fix

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
